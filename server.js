d300b422ac0f420a15df3a09be9993d3c883b98f
Nguyen Minh Phu <phu20032k1@gmail.com>
2026-01-09 05:04:08 +0700

    i

--- server.js
+++ server.js
@@ -121,9 +121,9 @@ mongoose
   });
 
 // ⭐ START SERVER
-const PORT = Number(process.env.PORT || 8080);
-app.listen(PORT, () => {
-  console.log("✔ Server running on port", PORT);
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
 
 
